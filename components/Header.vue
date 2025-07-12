5909b8e5e9b67ac02a43d22818d4edba28d48dbb
andkcode <akiricuk37@gmail.com>
2025-07-12 16:44:04 +0200

    feat: Implement basic layout with header and footer
    This commit introduces the initial layout structure for the application.
    
    - Created `Header.vue` and `Footer.vue` components to provide consistent header and footer sections across the app.
    - Implemented `default.vue` layout to wrap all pages with the header and footer.
    - Added placeholder pages for `admin`, `login`, and `blog/[slug]` to establish the initial routing structure.
    
    This setup provides a foundation for building out the application's UI and navigation.

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container bg-white w-full h-full">
+dsdsaadsdsadas
+    
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <UApp>
+        <Header />
+            <slot />
+        <Footer />
+    </UApp>
+</template>
+<script lang="ts" setup>
+import Header from '~/components/Header.vue'    
+import Footer from '~/components/Footer.vue'
+</script>
