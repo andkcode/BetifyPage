35744c6c6cdbd5c48158267815d59b9a861aecdf
andkcode <akiricuk37@gmail.com>
2025-08-04 12:33:16 +0200

    feat(guide): Add "How to deposit" guide page
    This commit introduces a new guide page explaining how to deposit funds.
    
    The page includes:
    
    - A title and introductory text.
    - Placeholder documentation content.
    
    This addition aims to provide users with clear instructions on depositing funds, enhancing the user experience and reducing support requests.

---
+++ pages/guide/get-started/how-to-deposite.vue
@@ -0,0 +1,22 @@
+<template>
+      <div class="flex w-full h-full flex-col">
+
+      <div class="flex flex-col items-start justify-start w-full pl-10 pt-10 space-y-2">
+        <h1 class="text-[1rem] font-bold text-blue-400">Get Started</h1>
+        <p class="text-black text-5xl font-semibold">How to deposit</p>
+      </div>
+
+      <div class="flex space-x-2 text-gray-500 px-10 py-10">
+        <p>Documentation - betify this is bullshit created by 2 Andrews and Max, we are not responsible for any issues with the website.Documentation - betify this is bullshit created by 2 Andrews and Max, we are not responsible for any issues with the website.</p>
+      </div>
+
+      </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
