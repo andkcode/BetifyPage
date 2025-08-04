42acfdece744b7204c8445252dde960ba65d3a3d
andkcode <akiricuk37@gmail.com>
2025-08-04 12:33:07 +0200

    feat(guide): Implement guide page layout
    This commit introduces the main layout structure for the guide section of the application.
    
    The layout consists of three main components:
    
    - `SideBar`: A navigation sidebar for the guide.
    - `NuxtPage`: The main content area, utilizing Nuxt's page rendering.
    - `RightSideBar`: A sidebar for supplementary information.
    
    The layout adapts to different screen sizes:
    
    - On large screens (lg), a horizontal layout with all three components visible is used.
    - On medium screens (md) and smaller, a vertical layout is used, hiding the sidebars.
    
    The page automatically redirects to the "Get Started" section upon initial load, providing a smoother user experience.

---
+++ pages/guide.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hidden lg:flex w-4/5 h-screen bg-[rgba(255,255,255,0.5)] justify-self-center self-center flex-row rounded-[1rem]">
+    <SideBar class=""/>
+
+  <div class="w-3/5 h-full">
+    <NuxtPage />  
+  </div>
+
+  <RightSideBar />
+
+  </div>
+
+  <div class="lg:hidden md:flex w-5/6 h-screen justify-self-center self-center flex-col">
+    <!-- <div class="w-full flex flex-row h-1/5"> 
+      <SideBar class="bg-[rgba(255,255,255,0.5)] rounded-[1rem] w-full"/>   
+      <RightSideBar class="bg-[rgba(255,255,255,0.5)] rounded-[1rem] w-full" />
+    </div> -->
+
+  <div class="bg-[rgba(255,255,255,0.5)] rounded-[1rem] h-full">
+    <NuxtPage />  
+  </div>
+
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  redirect: '/guide/get-started/what-is-betify'
+})
+</script>
