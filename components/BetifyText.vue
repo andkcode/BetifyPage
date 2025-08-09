3f937881c14d2c5ae0e97cd862fab046907d88cf
andkcode <akiricuk37@gmail.com>
2025-08-09 18:21:32 +0200

    feat: Implement BetifyText component
    This commit introduces the `BetifyText` component.
    
    The `BetifyText` component is a reusable Vue component designed to display text within a styled container. It utilizes a blue background with white text and rounded corners, providing a visually distinct element for highlighting specific text strings.
    
    The component accepts a `text` prop, allowing developers to easily customize the displayed content. It's styled using Tailwind CSS classes for consistent and maintainable styling.
    
    This component enhances the UI by providing a standardized way to present important text snippets.

---
+++ components/BetifyText.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <span class="px-[0.4rem] py-[0.2rem] mx-1 bg-blue-600 text-white rounded-[0.4rem] items-center justify-center flex flex-row w-fit">{{ text }}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  text: String
+})
+</script>
+
+<style>
+
+</style>
