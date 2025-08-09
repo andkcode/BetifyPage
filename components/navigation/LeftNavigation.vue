6207e47db22a04e2da3b21bb54c4456a043f9ddc
andkcode <akiricuk37@gmail.com>
2025-08-09 18:22:36 +0200

    feat(navigation): Implement LeftNavigation component
    This commit introduces the `LeftNavigation` component, a reusable UI element designed to provide consistent back navigation across the application.
    
    The component accepts `text` and `path` props to dynamically generate a link with a left arrow icon. This promotes code reusability and simplifies the creation of back navigation elements throughout the application.
    
    This initial implementation focuses on basic functionality and styling. Future enhancements may include additional styling options and accessibility improvements.

---
+++ components/navigation/LeftNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+      <NuxtLink :to="path" class="flex flex-row font-semibold items-center justify-center space-x-4 hover:text-blue-600">
+        <UIcon name="mingcute:left-fill" class="text-md" />
+        {{ text }}
+      </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  text: String,
+  path: String,
+})
+</script>
+
+<style>
+
+</style>
