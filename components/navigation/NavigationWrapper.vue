90c9889e2eafe111c0262eeb5dbc056ee87cb840
andkcode <akiricuk37@gmail.com>
2025-08-09 18:22:48 +0200

    feat(navigation): Introduce NavigationWrapper component for dynamic navigation links
    This commit introduces the `NavigationWrapper` component. This component serves as a container for left and right navigation links, enabling dynamic navigation based on provided `leftText`, `rightText`, `leftPath`, and `rightPath` props.
    
    The `NavigationWrapper` component enhances the user interface by providing a structured and visually appealing way to navigate between different sections or pages of the application. It promotes code reusability and simplifies the process of managing navigation elements across the application.

---
+++ components/navigation/NavigationWrapper.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="w-full space-y-2 flex flex-col mx-auto pb-2"> 
+      <span class="w-full h-[2px] bg-blue-200"></span>
+      <div class="flex flex-row items-center justify-between text-[0.9rem] my-2 mx-2">
+        <LeftNavigation :path="leftPath" :text="leftText" />
+        <RightNavigation :path="rightPath" :text="rightText" />
+      </div>
+    </section>
+</template>
+
+<script lang="ts" setup>
+import LeftNavigation from '~/components/navigation/LeftNavigation.vue';
+import RightNavigation from '~/components/navigation/RightNavigation.vue';
+defineProps({
+  leftText: String,
+  rightText: String,
+  leftPath: String,
+  rightPath: String
+})
+</script>
+
+<style>
+
+</style>
