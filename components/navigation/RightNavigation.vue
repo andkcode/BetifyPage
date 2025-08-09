2dea6026cdef80fb2acc102d4d1d47115eb15e31
andkcode <akiricuk37@gmail.com>
2025-08-09 18:22:42 +0200

    feat(navigation): Add RightNavigation component for forward navigation
    This commit introduces the `RightNavigation` component. This component provides a visually distinct and interactive element for forward navigation within the application.
    
    The component accepts `text` and `path` props, allowing it to display the navigation text and link to the specified route, respectively. A right-facing arrow icon is included to visually indicate forward movement.
    
    This enhances the user experience by providing a clear and intuitive way to navigate forward in the application flow.

---
+++ components/navigation/RightNavigation.vue
@@ -0,0 +1,17 @@
+<template>
+      <NuxtLink :to="path"  class="flex font-semibold flex-row items-center justify-center space-x-2 hover:text-blue-600">
+        {{ text }}
+        <UIcon name="mingcute:right-fill" class="text-md" />
+      </NuxtLink>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  text: String,
+  path: String,
+})
+</script>
+
+<style>
+
+</style>
